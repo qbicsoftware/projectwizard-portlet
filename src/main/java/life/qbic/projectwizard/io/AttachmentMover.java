d34f28c424bedec3529879985aa18a23022cd775
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2020-10-28 11:53:16 +0100

    add convenience functions

--- src/main/java/life/qbic/projectwizard/io/AttachmentMover.java
+++ src/main/java/life/qbic/projectwizard/io/AttachmentMover.java
@@ -3,18 +3,18 @@ package life.qbic.projectwizard.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.ProgressBar;
 import com.vaadin.ui.UI;
-
 import life.qbic.datamodel.attachments.AttachmentConfig;
 import life.qbic.projectwizard.processes.MoveUploadsReadyRunnable;
 import life.qbic.projectwizard.registration.UpdateProgressBar;
@@ -42,6 +42,61 @@ public class AttachmentMover {
     this.config = attachmentConfig;
   }
 
+  public void createAttachmentFromStringMoveAndMarker(String content, String fileName,
+      String description, String type, String user, String barcode) {
+    Date date = new java.util.Date();
+    String timeStamp =
+        new Timestamp(date.getTime()).toString().split(" ")[1].replace(":", "").replace(".", "");
+
+    AttachmentInformation attachment =
+        new AttachmentInformation(fileName, description, type, user, barcode, timeStamp);
+    Sardine sardine = SardineFactory.begin(config.getUser(), config.getPass());
+
+    String folder = attachment.getFolder();
+    byte[] data = content.getBytes(StandardCharsets.UTF_8);
+    try {
+      sardine.createDirectory(config.getUri() + timeStamp);
+
+      sardine.put(config.getUri() + folder + "/" + attachment.getFileName(), data);
+
+      byte[] metadata = createMetadataString(attachment).getBytes();
+      sardine.put(config.getUri() + folder + "/" + "metadata.txt", metadata);
+
+      Thread.sleep(1000);
+
+      createMarker(sardine, folder, attachment.getFileName());
+
+    } catch (Exception e1) {
+      e1.printStackTrace();
+      try {
+        sardine.shutdown();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void moveAttachment(Sardine sardine, AttachmentInformation attachment) {
+    byte[] data;
+    try {
+      sardine.createDirectory(config.getUri() + attachment.getFolder());
+
+      data = FileUtils.readFileToByteArray(new File(tmpFolder + attachment.getName()));
+      sardine.put(config.getUri() + attachment.getFolder() + "/" + attachment.getFileName(), data);
+
+      byte[] metadata = createMetadataString(attachment).getBytes();
+      sardine.put(config.getUri() + attachment.getFolder() + "/" + "metadata.txt", metadata);
+
+    } catch (Exception e1) {
+      e1.printStackTrace();
+      try {
+        sardine.shutdown();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
   /**
    * Moves attachments to the datamover folder.
    * 
@@ -67,24 +122,7 @@ public class AttachmentMover {
             current++;
             double frac = current * 1.0 / todo;
             UI.getCurrent().access(new UpdateProgressBar(bar, info, frac));
-            byte[] data;
-            try {
-              sardine.createDirectory(config.getUri() + a.getFolder());
-
-              data = FileUtils.readFileToByteArray(new File(tmpFolder + a.getName()));
-              sardine.put(config.getUri() + a.getFolder() + "/" + a.getFileName(), data);
-
-              byte[] metadata = createMetadataString(a).getBytes();
-              sardine.put(config.getUri() + a.getFolder() + "/" + "metadata.txt", metadata);
-
-            } catch (Exception e1) {
-              e1.printStackTrace();
-              try {
-                sardine.shutdown();
-              } catch (IOException e) {
-                e.printStackTrace();
-              }
-            }
+            moveAttachment(sardine, a);
           }
           UI.getCurrent().access(ready);
           UI.getCurrent().setPollInterval(-1);
@@ -104,12 +142,6 @@ public class AttachmentMover {
     }
   }
 
-  // public void folderTest(AttachmentInformation a) throws IOException {
-  // final Sardine sardine = SardineFactory.begin(config.getUser(), config.getPass());
-  // sardine.createDirectory(config.getUri() + a.getFolder());
-  // sardine.shutdown();
-  // }
-
   protected String createMetadataString(AttachmentInformation a) {
     String user = a.getUser();
     String info = a.getInfo().replace("\n", " ");
@@ -119,30 +151,32 @@ public class AttachmentMover {
         + type;
   }
 
+  private void createMarker(Sardine sardine, String folder, String fileName)
+      throws InterruptedException, IOException {
+    String prefix = ".MARKER_is_finished_";
+    String datafile = folder + "/" + fileName;
+    byte[] data = new byte[0];
+    int maxTries = 2;
+    while (maxTries > 0 && !sardine.exists(config.getUri() + datafile)) {
+      maxTries--;
+      Thread.sleep(100);
+    }
+    if (!sardine.exists(config.getUri() + datafile)) {
+      logger.error("Did not create marker file because folder " + datafile
+          + " was not found on the server.");
+      logger.error("Killing Sardine process.");
+      sardine.shutdown();
+    } else {
+      // data = FileUtils.readFileToByteArray(marker);
+      sardine.put(config.getUri() + prefix + folder, data);
+    }
+  }
+
   private void createMarkers(List<AttachmentInformation> attachments)
       throws InterruptedException, IOException {
     Sardine sardine = SardineFactory.begin(config.getUser(), config.getPass());
-    // File marker = new File(tmpFolder + "marker");
-    // if (!marker.exists())
-    // marker.createNewFile();
-    String prefix = ".MARKER_is_finished_";
     for (AttachmentInformation a : attachments) {
-      String datafile = a.getFolder() + "/" + a.getFileName();
-      byte[] data = new byte[0];
-      int maxTries = 2;
-      while (maxTries > 0 && !sardine.exists(config.getUri() + datafile)) {
-        maxTries--;
-        Thread.sleep(100);
-      }
-      if (!sardine.exists(config.getUri() + datafile)) {
-        logger.error("Did not create marker file because folder " + datafile
-            + " was not found on the server.");
-        logger.error("Killing Sardine process.");
-        sardine.shutdown();
-      } else {
-        // data = FileUtils.readFileToByteArray(marker);
-        sardine.put(config.getUri() + prefix + a.getFolder(), data);
-      }
+      createMarker(sardine, a.getFolder(), a.getFileName());
     }
     sardine.shutdown();
   }
